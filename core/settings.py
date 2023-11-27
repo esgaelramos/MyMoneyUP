15b2e875ca9513b62a6d05d04236918978ab56fd
Lorenzo Ferreyra <ferreyralorenzo2@gmail.com>
2023-11-26 21:34:34 -0300

    email_smtp

--- core/settings.py
+++ core/settings.py
@@ -113,7 +113,17 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email Backend
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env_settings['EMAIL_USER']
+EMAIL_HOST_PASSWORD = env_settings['EMAIL_PASSWORD']
