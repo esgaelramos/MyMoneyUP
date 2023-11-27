a047db7caf10ecdde66717608f2a55651a659fb1
Lorenzo Ferreyra <ferreyralorenzo2@gmail.com>
2023-11-26 22:05:14 -0300

    HTML Form Added

--- moneytracker/views.py
+++ moneytracker/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import View, TemplateView
-
+from django.core.mail import send_mail
+from django.http import HttpResponse
 # Create your views here.
 class Tracker(View):
     def get(self, request, *args, **kwargs):
@@ -21,3 +22,15 @@ class Contact(View):
         context={
         }
         return render(request, "moneytracker/contact.html", context)
+    def post(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            subject = request.POST.get('subject')
+            message = request.POST.get('message')
+            to_email = request.POST.get('to_email')
+            
+            send_mail(subject, message, 'tuemail@gmail.com', [to_email])
+            return HttpResponse("Correo enviado con éxito!")
+
+        return HttpResponse("Error: Este formulario solo acepta solicitudes POST.")
+
+
