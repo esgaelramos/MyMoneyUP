14a51453aa8fbda703a0d5e7ec58265a9b3e021d
Gael Ramos <esgaelramos@gmail.com>
2023-11-27 18:07:12 -0600

    add user model

--- moneytracker/models.py
+++ moneytracker/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=50, unique=True)
+    mail = models.EmailField(max_length=254, unique=True)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
