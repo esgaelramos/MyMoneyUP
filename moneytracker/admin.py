afff09867bc554c2a079350de5671301a72d4c7e
Lorenzo Ferreyra <ferreyralorenzo2@gmail.com>
2023-11-28 19:34:31 -0300

    Models Creation
    creacion de modelos para database

--- moneytracker/admin.py
+++ moneytracker/admin.py
@@ -1,5 +1,32 @@
 from django.contrib import admin
 
-from .models import User
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import Asset, Portfolio, Performance, Session
 
-admin.site.register(User)
+# Acá registro los modelos en el admin para poder verlos en el sitio de Django
+
+@admin.register(Asset)
+class AssetAdmin(admin.ModelAdmin):
+    list_display = ('name', 'symbol', 'type')
+
+@admin.register(Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):
+    list_display = ('user', 'quantity', 'acquisition_date')
+    list_filter = ('user', 'acquisition_date')
+
+@admin.register(Performance)
+class PerformanceAdmin(admin.ModelAdmin):
+    list_display = ('user', 'date', 'total_portfolio_value', 'performance')
+    list_filter = ('user', 'date')
+
+@admin.register(Session)
+class SessionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'login_date')
+    list_filter = ('user', 'login_date')
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser')
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
--- moneytracker/models.py
+++ moneytracker/models.py
@@ -1,13 +1,35 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
-
-
-class User(models.Model):
-    id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=50, unique=True)
-    mail = models.EmailField(max_length=254, unique=True)
-    status = models.BooleanField(default=False)
+# Notar que traemos el objeto User que crea Django por defecto
+## y luego lo citamos. por eso no creamos la clase usuario
+class Asset(models.Model):
+    name = models.CharField(max_length=255)
+    symbol = models.CharField(max_length=10)
+    type = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.username
+        return f"{self.name} ({self.symbol}) - {self.type}"
+
+class Portfolio(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    assets = models.ManyToManyField(Asset)
+    quantity = models.DecimalField(max_digits=18, decimal_places=8)
+    acquisition_date = models.DateField()
+
+    class Meta:
+        unique_together = ('user', 'quantity', 'acquisition_date')
+
+class Performance(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    days_to_send_email = models.IntegerField()
+    total_portfolio_value = models.DecimalField(max_digits=18, decimal_places=8)
+    performance = models.DecimalField(max_digits=5, decimal_places=2)
+
+    class Meta:
+        unique_together = ('user', 'date')
+
+class Session(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    login_date = models.DateField()
