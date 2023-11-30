31b05d7de6c72b80e62ce45a9c720bd54faf34ab
Lorenzo Ferreyra <ferreyralorenzo2@gmail.com>
2023-11-30 20:08:11 -0300

    Cambios en models.py

--- moneytracker/models.py
+++ moneytracker/models.py
@@ -1,13 +1,34 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+# Notar que traemos el objeto User que crea Django por defecto
+## y luego lo citamos. por eso no creamos la clase usuario
+class CustomUser(User):
+    suscribed = models.BooleanField(default=False)
 
-
-class User(models.Model):
-    id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=50, unique=True)
-    mail = models.EmailField(max_length=254, unique=True)
-    status = models.BooleanField(default=False)
+class Asset(models.Model):
+    name = models.CharField(max_length=255)
+    symbol = models.CharField(max_length=10)
+    type = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.username
+        return f"{self.name} ({self.symbol}) - {self.type}"
+
+class Portfolio(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    assets = models.ManyToManyField(Asset)
+    quantity = models.DecimalField(max_digits=18, decimal_places=8)
+    acquisition_date = models.DateField()
+
+    class Meta:
+        unique_together = ('user', 'quantity', 'acquisition_date')
+
+class Performance(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    date = models.DateField()
+    days_to_send_email = models.IntegerField()
+    total_portfolio_value = models.DecimalField(max_digits=18, decimal_places=8)
+
+    class Meta:
+        unique_together = ('user', 'date')
+
