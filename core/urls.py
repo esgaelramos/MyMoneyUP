d9bca876e63cec93cba67ac4b8f9ae29bc28580a
JorgeGarcia-Dev <georgegarciacode@outlook.com>
2023-12-04 18:54:41 -0600

    add: add term views, create test for views and urls

--- core/urls.py
+++ core/urls.py
@@ -17,10 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-# from .views import HomeView
+from .views import TermsView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",include("moneytracker.urls"))
+    path("",include("moneytracker.urls")),
+    path("terms/", TermsView.as_view(), name="terms"),
 ]
---
+++ tests/test_core/test_views.py
@@ -0,0 +1,22 @@
+"""
+View Tests for the core project
+
+This module contains unit tests that focus on the behavior of the views
+within the core project. It ensures that views render the expected templates,
+redirects occur as anticipated.
+
+Note:
+- For referencing URLs within these view tests, use the reverse function to get
+the URL from the named URL pattern.
+"""
+from django.test import TestCase
+from django.urls import reverse
+
+class TermsViewTest(TestCase):
+    """
+    Tests that the terms view is working correctly.
+    """
+    def test_terms_view_uses_correct_template(self):
+        response = self.client.get(reverse('terms'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'terms/terms.html')
