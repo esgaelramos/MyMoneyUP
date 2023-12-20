4a27513865dcb7f4fb81869fc59daa445cf39b21
Camila Pua <camilapua7@gmail.com>
2023-12-20 11:23:07 -0500

    Fix some format errors in tests files for linter Update max-line-length to 79 in .flake8 file Delete unnecessary parameters in pre commit config file

--- tests/test_core/test_utils.py
+++ tests/test_core/test_utils.py
@@ -1,22 +1,17 @@
-"""
-Tests for module `core.utils`.
-"""
+"""Tests for module `core.utils`."""
 from core.utils import str_to_bool
 
 
 def test_str_to_bool_true_cases():
-    """
-    Test the str_to_bool function for values that should return True.
-    """
+    """Test the str_to_bool function for values that should return True."""
     true_values = ['True', 'true', 'TRUE', 'TrUe']
 
     for value in true_values:
         assert str_to_bool(value) == True
 
+
 def test_str_to_bool_false_cases():
-    """
-    Test the str_to_bool function for values that should return False.
-    """
+    """Test the str_to_bool function for values that should return False."""
     false_values = ['False', 'false', 'FALSE', 'FaLsE', '', 'NotTrue', '123']
 
     for value in false_values:
--- tests/test_moneytracker/test_models_integration.py
+++ tests/test_moneytracker/test_models_integration.py
@@ -1,7 +1,9 @@
-"""
-Test cases for the models of the moneytracker app.
-Requiere a database (default: sqlite3) so this tests are integration tests.
-Only here check the 'contrains' conflicts and working. 
+"""Test cases for the models of the moneytracker app.
+
+Requiere a database (default: sqlite3) so this tests are integration
+tests.
+
+Only here check the 'contrains' conflicts and working.
 """
 import datetime
 
@@ -9,16 +11,19 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from django.core.management import call_command
 from django.db.utils import IntegrityError
-from moneytracker.models import Asset, CustomUser, Performance, Portfolio, PortfolioAsset
+from moneytracker.models import Asset, CustomUser, Performance
+from moneytracker.models import Portfolio, PortfolioAsset
+
 
 class CustomUserModelIntegrationTest(TestCase):
-    """
-    Test that a CustomUser object was created susccessfully
-    """
+    """Test that a CustomUser object was created susccessfully."""
+
     def setUp(self):
+        """Set up the test fixture before exercising it."""
         self.django_user = User.objects.create(username='Django User')
 
     def test_customuser_suscribed_creation(self):
+        """Test a suscribed CustomUser object was created susccessfully."""
         customuser_example = CustomUser.objects.create(user=self.django_user)
 
         self.assertEqual(customuser_example.user.username, 'Django User')
@@ -26,7 +31,9 @@ class CustomUserModelIntegrationTest(TestCase):
         self.assertEqual(customuser_example.user, self.django_user)
 
     def test_customuser_not_suscribed_creation(self):
-        customuser_example = CustomUser.objects.create(suscribed=False, user=self.django_user)
+        """Test a not suscribed CustomUser object was created susccessfully."""
+        customuser_example = CustomUser.objects.create(
+            suscribed=False, user=self.django_user)
 
         self.assertEqual(customuser_example.user.username, 'Django User')
         self.assertEqual(customuser_example.suscribed, False)
@@ -34,13 +41,13 @@ class CustomUserModelIntegrationTest(TestCase):
 
 
 class AssetModelIntegrationTest(TestCase):
-    """
-    Test that a Asset object was created susccessfully
-    """
+    """Test that a Asset object was created susccessfully."""
+
     def test_asset_creation(self):
+        """Test that a Asset object was created susccessfully."""
         asset_example = Asset.objects.create(name='Test Name',
-                                            symbol='TestSymbol',
-                                            type="Test Type")
+                                             symbol='TestSymbol',
+                                             type="Test Type")
 
         self.assertEqual(asset_example.name, 'Test Name')
         self.assertEqual(asset_example.symbol, 'TestSymbol')
@@ -48,30 +55,34 @@ class AssetModelIntegrationTest(TestCase):
 
 
 class PortfolioModelIntegrationTest(TestCase):
-    """
-    Test that a Portfolio object was created susccessfully
-    """
+    """Test that a Portfolio object was created susccessfully."""
+
     def setUp(self):
+        """Set up the test fixture before exercising it."""
         django_user = User.objects.create(username='Django User')
         self.customuser_example = CustomUser.objects.create(user=django_user)
-        
+
     def test_portfolio_creation(self):
-        portfolio_example = Portfolio.objects.create(user=self.customuser_example)
+        """Test that a Portfolio object was created susccessfully."""
+        portfolio_example = Portfolio.objects.create(
+            user=self.customuser_example)
 
         self.assertEqual(portfolio_example.user.user.username, 'Django User')
 
 
 class PortfolioAssetModelIntegrationTest(TestCase):
-    """"
-    Test that a PortfolioAsset object was created susccessfully
-    """
+    """Test that a PortfolioAsset object was created susccessfully."""
+
     def setUp(self):
+        """Set up the test fixture before exercising it."""
         django_user = User.objects.create(username='DjangoUser')
         custom_user = CustomUser.objects.create(user=django_user)
-        self.asset = Asset.objects.create(name='Test Asset', symbol='TA', type='Test Type')
+        self.asset = Asset.objects.create(
+            name='Test Asset', symbol='TA', type='Test Type')
         self.portfolio = Portfolio.objects.create(user=custom_user)
 
     def test_portfolio_asset_creation(self):
+        """Test that a PortfolioAsset object was created susccessfully."""
         acquisition_date = datetime.date.today()
         portfolio_asset_example = PortfolioAsset.objects.create(
             portfolio=self.portfolio,
@@ -80,12 +91,15 @@ class PortfolioAssetModelIntegrationTest(TestCase):
             acquisition_date=acquisition_date
         )
 
-        self.assertEqual(portfolio_asset_example.portfolio.user.user.username, 'DjangoUser')
+        self.assertEqual(
+            portfolio_asset_example.portfolio.user.user.username, 'DjangoUser')
         self.assertEqual(portfolio_asset_example.asset.name, 'Test Asset')
         self.assertEqual(portfolio_asset_example.quantity, 10.0)
-        self.assertEqual(portfolio_asset_example.acquisition_date, datetime.date.today())
+        self.assertEqual(portfolio_asset_example.acquisition_date,
+                         datetime.date.today())
 
     def test_unique_portfolio_asset_contraint(self):
+        """Test that a PortfolioAsset object it's unique."""
         acquisition_date = datetime.date.today()
         PortfolioAsset.objects.create(
             portfolio=self.portfolio,
@@ -93,7 +107,8 @@ class PortfolioAssetModelIntegrationTest(TestCase):
             quantity=10.0,
             acquisition_date=acquisition_date
         )
-        # Try create a second object PortfolioAsset with same values and catch the exception
+        # Try create a second object PortfolioAsset with same values and
+        # catch the exception
         with self.assertRaises(IntegrityError):
             PortfolioAsset.objects.create(
                 portfolio=self.portfolio,
@@ -101,35 +116,46 @@ class PortfolioAssetModelIntegrationTest(TestCase):
                 quantity=10.0,
                 acquisition_date=acquisition_date
             )
-        
+
 
 class PerformanceModelIntegrationTest(TestCase):
-    """
-    Test that a Performance object was created successfully
-    """
+    """Test that a Performance object was created successfully."""
+
     def setUp(self):
+        """Set up the test fixture before exercising it."""
         django_user = User.objects.create(username='TestUser')
         self.custom_user = CustomUser.objects.create(user=django_user)
 
     def test_performance_creation(self):
-        performance_example = Performance.objects.create(user=self.custom_user, days_to_send_email=7)
+        """Test that a Performance object was created successfully."""
+        performance_example = Performance.objects.create(
+            user=self.custom_user, days_to_send_email=7)
 
         self.assertEqual(performance_example.user.user.username, 'TestUser')
         self.assertEqual(performance_example.days_to_send_email, 7)
 
     def test_unique_performance_constraint(self):
+        """Test that a PortfolioAsset object it's unique."""
         Performance.objects.create(user=self.custom_user, days_to_send_email=7)
 
-        # Try create a second object Performace with same values and catch the exception
+        # Try create a second object Performace with same values and catch
+        # the exception
         with self.assertRaises(IntegrityError):
-            Performance.objects.create(user=self.custom_user, days_to_send_email=7)
+            Performance.objects.create(
+                user=self.custom_user, days_to_send_email=7)
 
 
 class CheckLoadDataTest(TestCase):
+    """Test that the data was loaded correctly.
+
+    From the data_initial.json fixture!
     """
-    Test that the data was loaded correctly from the data_initial.json fixture!
-    """
+
     def test_loaddata(self):
+        """Test that the data was loaded correctly.
+
+        From the data_initial.json fixture!
+        """
         # Check that the database is empty at the beginning
         self.assertEqual(Asset.objects.count(), 0)
         self.assertEqual(CustomUser.objects.count(), 0)
