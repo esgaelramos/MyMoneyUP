a8d5388a41915bc8a0ab7ed540fd778799707e6e
Gael Ramos <esgaelramos@gmail.com>
2023-12-26 16:04:54 -0600

    lint: fix formats, docs and style code for the script sync assets and tests manage, add docstrings for the inits

---
+++ tests/test_core/__init__.py
@@ -0,0 +1,6 @@
+"""Test suite for the core package of the Django Project.
+
+This subpackage includes tests for the core functionalities of the project,
+covering essential modules and features. The tests are designed to be
+independent of the another subpackage, so that they can be run separately.
+"""
