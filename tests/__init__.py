d4df3fda5ec2a68d47cd07f257ff6d2f9798a0c1
Gael Ramos <esgaelramos@gmail.com>
2023-12-26 16:04:54 -0600

    lint: fix formats, docs and style code for the script sync assets and tests manage, add docstrings for the inits

---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+"""Test suite for the MyMoneyUP Project.
+
+This package contains various test modules for testing the different
+components of the django project and application. The tests are run
+using pytest and combine unit and integration tests.
+"""
--- tests/test_manage.py
+++ tests/test_manage.py
@@ -4,15 +4,21 @@ Tests for module `manage`.
 In this case, we are testing the manage.py script, so we need to check
 if it can be imported and if it runs correctly.
 """
-import pytest
 import subprocess
-
 from unittest.mock import patch
+
+import pytest
 
 
 def test_manage():
+    """
+    Test the importability of the main function from manage.py.
+
+    This test checks if the main function from manage.py can be imported
+    without errors, indicating the script is structured correctly.
+    """
     try:
-        # Intenta importar la función principal de manage.py
+        # try importing the main function from manage.py
         from manage import main
         assert main is not None
     except Exception as e:
@@ -20,11 +26,18 @@ def test_manage():
 
 
 def test_manage_raise_exception():
-    # use 'execute_from_command_line' and not 'import_module', becasue 'import' preloads the module and we want to test!
+    """
+    Test manage.py for proper error handling when Django is not importable.
+
+    This test mocks a failed Django import to ensure that manage.py
+    handles such exceptions as expected.
+    """
+    # use 'execute_from_command_line' and not 'import_module',
+    # becasue 'import' preloads the module and we want to test!
     def mock_execute_from_command_line(*args, **kwargs):
         raise ImportError("Couldn't import Django")
 
-    with patch("django.core.management.execute_from_command_line", mock_execute_from_command_line):
+    with patch("django.core.management.execute_from_command_line", mock_execute_from_command_line):  # noqa: E501
         with pytest.raises(ImportError) as execinfo:
             from manage import main
             main()
@@ -33,5 +46,16 @@ def test_manage_raise_exception():
 
 
 def test_manage_run():
-    result = subprocess.run(["python", "manage.py", "check"], capture_output=True)
-    assert result.returncode == 0, f"manage.py check failed with output: {result.stderr.decode()}"
+    """
+    Test the execution of manage.py's check command.
+
+    This test executes 'manage.py check' using subprocess to ensure that
+    the script runs correctly and exits without errors.
+    """
+    result = subprocess.run(
+        ["python", "manage.py", "check"],
+        capture_output=True
+    )
+
+    assert result.returncode == 0, \
+        f"manage.py check failed with output: {result.stderr.decode()}"
