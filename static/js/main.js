9771db2ee53292efd1b9305294439de16567be72
AndriwRC <randriw4@gmail.com>
2024-01-17 17:06:37 -0500

    update JS on currency selection feature

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,35 @@
 console.log("Prueba de inicio")
 
+/* Navigation */
+
 function homePage() {
     console.log('Home');
 
+    // Muestra la entrada de correo electrónico y el botón falso de suscripción
     titleTracker1.classList.remove('inactive');
-    titleTracker2.classList.add('inactive');
     emailSelectionDiv.classList.remove('inactive');
+
+    // Oculta la selección de monedas y el botón real de suscripción
+    titleTracker2.classList.add('inactive');
     currencySelectionDiv.classList.add('inactive');
 }
+
+function currencySelection() {
+    // Oculta la entrada de correo electrónico y el botón falso de suscripción
+    emailSelectionDiv.classList.add('inactive');
+    banner.classList.add('inactive');
+    titleTracker1.classList.add('inactive');
+
+    // Muestra la selección de monedas y el botón real de suscripción
+    currencySelectionDiv.classList.remove('inactive');
+    submitBtn.classList.remove('inactive');
+    titleTracker2.classList.remove('inactive');
+
+    // Actualiza el texto del botón según la cantidad de checkboxes seleccionados
+    updateButtonCount();
+}
+
+/* Email Verification */
 
 function checkEmailValidity() {
     // Verificar si el correo electrónico es válido
@@ -41,7 +63,7 @@ async function checkEmailExistence(domain, email) {
             alert('This email is already registered.');
         } else {
             // Continuar con la selección de monedas si el correo electrónico no existe
-            proceedToShowCurrencySelection();
+            currencySelection();
         }
 
     } catch (error) {
@@ -54,104 +76,85 @@ function getCsrfToken() {
     return document.querySelector('[name="csrfmiddlewaretoken"]').value;
 }
     
-    function proceedToShowCurrencySelection() {
-        // Oculta la entrada de correo electrónico y el botón falso de suscripción
-        document.getElementById('emailSelection').style.display = 'none';
-        document.getElementById('banner').style.display = 'none';
-        document.getElementById('titleTracker1').style.display = 'none';
-    
-        // Muestra la selección de monedas y el botón real de suscripción
-        currencySelectionDiv.style.display = 'block';
-        submitBtn.style.display = 'block';
-        titleTracker2.style.display = 'block';
-    
-        // Actualiza el texto del botón según la cantidad de checkboxes seleccionados
+
+/* Utils */
+
+// Función para actualizar el texto del botón de envío según la cantidad de checkboxes seleccionados
+function updateButtonCount() {
+    const selectedCount = checkboxes.length ? `Track ${document.querySelectorAll('input[name="assets"]:checked').length} Currencies` : 'Select Currencies';
+    submitBtn.textContent = selectedCount;
+}
+
+/* Event Listeners */
+
+// Añade un evento de cambio a cada checkbox para actualizar el conteo al cambiar su estado
+checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener('change', () => {
+        // Actualizar el conteo de checkboxes seleccionados
         updateButtonCount();
-    }
-    
-
-    // Función para actualizar el texto del botón de envío según la cantidad de checkboxes seleccionados
-    function updateButtonCount() {
-        var selectedCount = checkboxes.length ? `Track ${document.querySelectorAll('input[name="assets"]:checked').length} Currencies` : 'Select Currencies';
-        submitBtn.textContent = selectedCount;
-    }
-
-    // Añade un evento de cambio a cada checkbox para actualizar el conteo al cambiar su estado
-    checkboxes.forEach(function(checkbox) {
-        checkbox.addEventListener('change', function() {
-            // Actualizar el conteo de checkboxes seleccionados
-            updateButtonCount();
-            
-            // Cambiar la visibilidad de los SVGs
-            var uncheckedSVG = checkbox.nextElementSibling.querySelector('.unchecked');
-            var checkedSVG = checkbox.nextElementSibling.querySelector('.checked');
-            var currencyItem = checkbox.closest('.currency-item');
-            
-            if (checkbox.checked) {
-                uncheckedSVG.style.display = 'none';
-                checkedSVG.style.display = 'block';
-                currencyItem.classList.add('selected');
-            } else {
-                uncheckedSVG.style.display = 'block';
-                checkedSVG.style.display = 'none';
-                currencyItem.classList.remove('selected');
-            }
-        });
-    });
-
-    // Manejar el evento submit del formulario
-    subscribeForm.addEventListener('submit', function(event) {
-        // Verificar si la selección de monedas está oculta y si el campo de correo electrónico tiene el foco
-        if (currencySelectionDiv.style.display === 'none' && document.activeElement === emailInput) {
-            event.preventDefault(); // Prevenir que el formulario se envíe (cuando se presiona enter en el campo de correo electrónico)
-            showCurrencySelection(); // Mostrar la selección de monedas
-        }
-        // No es necesario un else aquí, si el display no está none, el formulario se enviará normalmente???
-    });
-
-    var loadMoreButton = document.getElementById('loadMoreButton');
-    var allCurrencyItems = document.querySelectorAll('.currency-item');
-    var itemsToShow = 2; // Número inicial de elementos a mostrar
-
-    // Inicialmente oculta todos los elementos excepto los dos primeros
-    allCurrencyItems.forEach(function(item, index) {
-        if (index >= itemsToShow) {
-            item.style.display = 'none';
+        
+        // Cambiar la visibilidad de los SVGs
+        const uncheckedSVG = checkbox.nextElementSibling.querySelector('.unchecked');
+        const checkedSVG = checkbox.nextElementSibling.querySelector('.checked');
+        const currencyItem = checkbox.closest('.currency-item');
+        
+        if (checkbox.checked) {
+            uncheckedSVG.classList.add('inactive');
+            checkedSVG.classList.remove('inactive');
+            currencyItem.classList.add('selected');
+        } else {
+            uncheckedSVG.classList.remove('inactive');
+            checkedSVG.classList.add('inactive');
+            currencyItem.classList.remove('selected');
         }
     });
+});
 
-    loadMoreButton.addEventListener('click', function() {
-        // Incrementa la cantidad de elementos a mostrar
-        itemsToShow += 2; // Ajusta este número según tus necesidades
-        // Muestra los elementos según el nuevo límite
-        allCurrencyItems.forEach(function(item, index) {
-            if (index < itemsToShow) {
-                item.style.display = 'block';
-            }
-        });
-        // Ocultar el botón mismo
-        loadMoreButton.style.display = 'none'
+// Manejar el evento submit del formulario
+subscribeForm.addEventListener('submit', (event) => {
+    // Verificar si la selección de monedas está oculta y si el campo de correo electrónico tiene el foco
+    if (currencySelectionDiv.classList.contains('inactive') && document.activeElement === emailInput) {
+        event.preventDefault(); // Prevenir que el formulario se envíe (cuando se presiona enter en el campo de correo electrónico)
+        checkEmailValidity(); // Verificar email
+    }
+    // No es necesario un else aquí, si el display no está none, el formulario se enviará normalmente???
+});
 
+let itemsToShow = 2; // Número inicial de elementos a mostrar
+
+// Inicialmente oculta todos los elementos excepto los dos primeros
+allCurrencyItems.forEach((item, index) => {
+    if (index >= itemsToShow) {
+        item.classList.add('inactive');
+    }
+});
+
+loadMoreButton.addEventListener('click', () => {
+    // Incrementa la cantidad de elementos a mostrar
+    itemsToShow += 2; // Ajusta este número según tus necesidades
+    // Muestra los elementos según el nuevo límite
+    allCurrencyItems.forEach((item, index) => {
+        if (index < itemsToShow) {
+            item.classList.remove('inactive');
+        }
     });
+    // Ocultar el botón mismo
+    loadMoreButton.classList.add('inactive');
+});
 
-    var searchInput = document.getElementById('searchInput');
-    searchInput.addEventListener('input', function() {
-        var searchText = searchInput.value.toLowerCase();
+searchInput.addEventListener('input', () => {
+    const searchText = searchInput.value.toLowerCase();
 
-        allCurrencyItems.forEach(function(item) {
-            var assetName = item.querySelector('label').textContent.toLowerCase();
-            if (assetName.includes(searchText)) {
-                item.style.display = 'block';
-            } else {
-                item.style.display = 'none';
-            }
-        });
+    allCurrencyItems.forEach((item) => {
+        const assetName = item.querySelector('label').textContent.toLowerCase();
+        if (assetName.includes(searchText)) {
+            item.classList.remove('inactive');
+        } else {
+            item.classList.add('inactive');
+        }
     });
+});
 
-    // Inicializa el estado del botón de envío
-    updateButtonCount();
-
-// Event Listeners
 emailBtn.addEventListener('click', checkEmailValidity);
 
 window.addEventListener('DOMContentLoaded', homePage, false);
